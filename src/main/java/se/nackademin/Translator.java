852ed4821c4bf7e81c9235c14441ba555a70d577
Tomas Gaal <tomas.gaal@yh.nackademin.se>
2016-07-20 17:46:22 +0200

    Temporary commit, new translator.java created.
    Lot unfinished! A sloppy update!

---
+++ src/main/java/se/nackademin/Translator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.nackademin;
+
+/**
+ *
+ * @author tomas
+ */
+public class WordList {
+    private String[] adjective = {"stor", "liten", "stark", "svag", "mjuk", "hård", "snabb", "vacker", "ljus", "mörk"};
+    private String[] noun = {"en lönehöjning", "en lönesänkning", "en fotboja", "en katt", "en hund", "ett hus", "ett barn", "ett elstängsel", "en dator", "ett golv"};
+    private String[] verb = {"spring", "ljuga", "flyga", "se", "vara", "äta", "mäta", "gå", "röra", "resa"};
+    private String[] wordList = {};
+}
+public class Translator {
+    String fortune_text = "";
+    int a = calculateA();
+    String stringA = wordList(a);
+    int b = calculateB();
+    String stringB = wordList(b);
+        
+             
+    String fortune_text_template = "Din framtid är" + stringA ""
+    
+}
+public int translate(){
+    
+}
